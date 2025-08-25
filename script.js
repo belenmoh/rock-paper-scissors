f9386ac459ca026fdf17f7f96481ac0af356719a
belenmoh <belenbubumoh@gmail.com>
2025-08-25 21:07:53 +0300

    added some style and animations

---
+++ script.js
@@ -0,0 +1,4 @@
+const options = ["rock", "paper", "scissors"]
+const playerdisplay = document.getElementsByClassName("playerdisplay");
+const computerdisplay = document.getElementsByClassName("computerdisplay");
+const result = document.getElementsByClassName("result")
